54ac8b3a02bdea750bb1f2aa5b0eff30e92b6251
Ben Strobel <benedikt.strobel@tum.de>
2025-02-27 01:48:44 +0100

    Remove redundant folder structure for plugin kinds and require plugin kind as first part of id

--- tools/invariant_checker/paths.ts
+++ tools/invariant_checker/paths.ts
@@ -1,28 +1,39 @@
 import { RootManifestPluginEntry } from "./schema.ts";
 
-export const manifestFolderPath = "../../manifests"
-export const rootManifestPath = `${manifestFolderPath}/plugins.toml`
+export const manifestFolderPath = "../../manifests";
+export const rootManifestPath = `${manifestFolderPath}/plugins.toml`;
 
-function getPluginKindFolderRepresentation(kind: "App" | "MediaProvider" | "Transcriber"): string {
-    switch(kind) {
-        case "App": return "apps"
-        case "MediaProvider": return "media_providers"
-        case "Transcriber": return "transcribers"
-    }
+export function getPluginKindFolderRepresentation(
+  kind: "App" | "MediaProvider" | "Transcriber",
+): string {
+  switch (kind) {
+    case "App":
+      return "app";
+    case "MediaProvider":
+      return "media-provider";
+    case "Transcriber":
+      return "transcriber";
+  }
 }
 
 export function getPluginFolderPath(pluginEntry: RootManifestPluginEntry) {
-  return `${manifestFolderPath}/${getPluginKindFolderRepresentation(pluginEntry.kind)}/${pluginEntry.id}`
+  return `${manifestFolderPath}/${pluginEntry.id}`;
 }
 
 export function getPluginManifestPath(pluginEntry: RootManifestPluginEntry) {
   return `${getPluginFolderPath(pluginEntry)}/plugin.toml`;
 }
 
-export function getPluginVerionFolderPath(pluginEntry: RootManifestPluginEntry, version: string) {
-  return `${getPluginFolderPath(pluginEntry)}/${version}`
+export function getPluginVerionFolderPath(
+  pluginEntry: RootManifestPluginEntry,
+  version: string,
+) {
+  return `${getPluginFolderPath(pluginEntry)}/${version}`;
 }
 
-export function getPluginVerionManifestPath(pluginEntry: RootManifestPluginEntry, version: string) {
+export function getPluginVerionManifestPath(
+  pluginEntry: RootManifestPluginEntry,
+  version: string,
+) {
   return `${getPluginVerionFolderPath(pluginEntry, version)}/version.toml`;
 }
